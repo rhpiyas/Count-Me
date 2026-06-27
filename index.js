760da05e508319c52f13cf24c643c97be0e11e60
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-06-27 21:28:14 +0600

    Add reset functionality to count with button2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document
 
 let x = document.getElementsByClassName("count")[0]
 
+let bt2 = document.getElementsByClassName("button2")[0]
+
 let count = 0;
 
 function increase()
@@ -9,6 +11,12 @@ function increase()
     x.innerText = count++;
 }
 
+function cl()
+{
+    x.innerHTML = count = 0
+}
+
 let a = document.getElementsByClassName("button")[0]
 
 a.onclick = increase;
+bt2.onclick = cl
