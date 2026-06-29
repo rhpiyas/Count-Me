d489a0c562d6009dd7713f736c26520188b2f5cb
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-06-29 17:05:58 +0600

    Implements the funcion for Decrease button

--- index.js
+++ index.js
@@ -20,7 +20,28 @@ function cl()
     x.style.color = "white";
 }
 
+function dec()
+{
+    if(count <= 0)
+    {
+        count = 0;
+    }
+    else
+    {
+        x.innerHTML = --count;
+    }
+    
+    let hue = (count % 10) * 36;
+
+    x.style.color = `hsl(${hue}, 100%, 50%)`;
+}
+
+
 let a = document.getElementsByClassName("button")[0];
+
+let bt3 = document.getElementsByClassName("button3")[0];
+
 
 a.onclick = increase;
 bt2.onclick = cl;
+bt3.onclick = dec;
